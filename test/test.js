a401cbabe905793b0a61c66fa150bd083ecbb3d5
Dan Delany <daniel.t.delany@jpl.nasa.gov>
2025-02-10 10:48:27 -0800

    improve tests to report validation errors

--- test/test.js
+++ test/test.js
@@ -4,30 +4,43 @@ import { readdirSync, readFileSync } from 'fs';
 import { exit } from 'process';
 
 function test() {
-  const validate = new Ajv({ strict: false }).compile(schema);
+  const ajv = new Ajv({
+    strict: false,
+    verbose: true,
+  });
+  const validate = ajv.compile(schema);
   const validSeqJsonPath = './test/valid-seq-json';
   const validSeqJsonFiles = readdirSync(validSeqJsonPath);
   const invalidSeqJsonPath = './test/invalid-seq-json';
   const invalidSeqJsonFiles = readdirSync(invalidSeqJsonPath);
-  const errors = [];
+  const failures = [];
 
   // Valid Seq JSON.
   for (const validSeqJsonFile of validSeqJsonFiles) {
     const validSeqJson = readFileSync(`${validSeqJsonPath}/${validSeqJsonFile}`).toString();
     const valid = validate(JSON.parse(validSeqJson));
-    if (!valid) errors.push(`${validSeqJsonFile} should be valid`);
+    if (!valid) {
+      // most relevant errors tend to be at the bottom - reverse list
+      const errors = validate.errors.reverse();
+      console.error(`❌ Failed to validate ${validSeqJsonFile} - ${errors.length} errors:`);
+      console.log(errors);
+      failures.push(`${validSeqJsonFile} should be valid`);
+    }
   }
 
   // Invalid Seq JSON.
   for (const invalidSeqJsonFile of invalidSeqJsonFiles) {
     const invalidSeqJson = readFileSync(`${invalidSeqJsonPath}/${invalidSeqJsonFile}`).toString();
     const valid = validate(JSON.parse(invalidSeqJson));
-    if (valid) errors.push(`${invalidSeqJsonFile} should be invalid`);
+    if (valid) {
+      console.error(`❌ Failed to invalidate ${invalidSeqJsonFile} - expected errors`);
+      failures.push(`${invalidSeqJsonFile} should be invalid`);
+    }
   }
 
-  if (errors.length) {
-    console.log('❌ Some tests failed...');
-    console.error(errors);
+  if (failures.length) {
+    console.log(`❌ ${failures.length} tests failed...`);
+    console.error(failures);
     exit(1);
   } else {
     console.log('✅ All tests passed!');
